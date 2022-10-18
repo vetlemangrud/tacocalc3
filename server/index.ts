5b3afc77cb0b83ed62f3358b524d152f71e2419b
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-10-18 20:34:28 +0200

    Sockets ONLINE

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import { serve } from "https://deno.land/std@0.150.0/http/server.ts";
+import { Server } from "https://deno.land/x/socket_io@0.1.1/mod.ts";
+
+const io = new Server({
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  socket.emit("hello", "world");
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  });
+});
+
+await serve(io.handler(), {
+  port: 3000,
+});
