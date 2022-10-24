4005203fe8a15e0e5332aaa11b6102e6fa073486
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-10-24 12:47:35 +0200

    Modal

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,11 +2,28 @@
   import HiveMindControl from "./lib/HiveMindControl.svelte";
   import IngredientList from "./lib/IngredientList.svelte";
   import { personCount } from "./stores/inputStatesStore";
+  import spinningTaco from "./assets/spinningTaco.gif";
+  import tacoStill from "./assets/tacoStill.gif";
+  import { roomStore } from "./stores/roomStore";
+  import { writable } from "svelte/store";
+  import Modal, { bind } from "svelte-simple-modal";
+  const modal = writable(null);
+  const showModal = () => modal.set(bind(HiveMindControl, {}));
+
+  let y;
+  $: smallHeader = y > 100;
 </script>
 
 <header>
+  <Modal show={$modal}>
+    <img
+      src={$roomStore ? spinningTaco : tacoStill}
+      alt="Taco"
+      on:click={showModal}
+    />
+  </Modal>
+  <p>{$roomStore ? "" : "Trykk på tacoen for å bli med i singulatiteten"}</p>
   <h1>Tacokalkulator 3</h1>
-  <HiveMindControl />
 </header>
 <main>
   <div>
@@ -15,3 +32,4 @@
   </div>
   <IngredientList />
 </main>
+<svelte:window bind:scrollY={y} />
