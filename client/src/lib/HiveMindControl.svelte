09a4a5a444aa48d668522af0b98dd83cbd2609e1
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-10-24 22:12:46 +0200

    Force uppercase on room input

--- client/src/lib/HiveMindControl.svelte
+++ client/src/lib/HiveMindControl.svelte
@@ -2,13 +2,16 @@
   import { roomStore } from "../stores/roomStore";
   import { createRoom, joinRoom } from "../util/socket";
   let roomToJoin = "";
+  const makeRoomUpper = () => {
+    roomToJoin = roomToJoin.toUpperCase();
+  };
 </script>
 
 <div>
   <h2>Tacolink</h2>
   {#if $roomStore === ""}
     <button on:click={() => createRoom()}>Create room</button>
-    <input type="text" bind:value={roomToJoin} />
+    <input type="text" bind:value={roomToJoin} on:input={makeRoomUpper} />
     <button on:click={() => joinRoom(roomToJoin)}>Join room</button>
   {:else}
     <p>Room: {$roomStore}</p>
