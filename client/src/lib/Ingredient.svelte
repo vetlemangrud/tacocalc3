9e0a91d77c7f48d9e00aaec037cdf9227cce7345
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-10-29 21:31:11 +0200

    Toggle checkbox on text press

--- client/src/lib/Ingredient.svelte
+++ client/src/lib/Ingredient.svelte
@@ -5,12 +5,26 @@
 </script>
 
 <div>
-  <p>
-    <input
-      type="checkbox"
-      bind:checked={$checkedState[ingredient.entallsform]}
-    />
-    {Math.floor(antall) || 0}
-    {antall === 1 ? ingredient.entallsform : ingredient.flertallsform}
-  </p>
+  <input type="checkbox" bind:checked={$checkedState[ingredient.entallsform]} />
+  {Math.floor(antall) || 0}
+  <span
+    on:click={() =>
+      checkedState.update((state) => {
+        console.log(state);
+
+        state[ingredient.entallsform] = !state[ingredient.entallsform];
+        return state;
+      })}
+    >{antall === 1 ? ingredient.entallsform : ingredient.flertallsform}</span
+  >
 </div>
+
+<style>
+  div {
+    padding: 0.5rem;
+    font-size: large;
+  }
+  input {
+    transform: scale(1.5);
+  }
+</style>
